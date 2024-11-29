e034d8853fa98af9de3b923edbc56507f705c0c9
Glitch (glitch-hello-node) <none>
2024-11-29 22:13:00 +0000

    🌑🛬 Checkpoint ./package.json:70407151/271 ./src/app.js:70407151/3665 ./src/config/database.js:70407151/333

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = require('./routes/index');
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: '108.181.92.76',
+  user: 'promptexecusr',
+  password: '@5G5l9c1',
+  database: 'nome_do_banco',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;

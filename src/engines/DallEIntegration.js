98afe57743a5e1fe4006095c7a0a881541fd7c99
Glitch (glitch-hello-node) <none>
2024-12-18 08:57:13 +0000

    🐢💕 Checkpoint ./src/engines/DallEIntegration.js:70407151/3612

--- src/engines/DallEIntegration.js
+++ src/engines/DallEIntegration.js
@@ -1,17 +1,20 @@
 const axios = require('axios');
+const ImageRepoAPI = require('../services/ImageRepoService'); // Ajuste o caminho para a classe ImageRepoAPI
+
 
 module.exports = {
   async process(prompt, model,modelParameters) {
     try {
       
-      modelParameter
+      modelParameters = modelParameters ? modelParameters : {};
+      
       const response = await axios.post(
         'https://api.openai.com/v1/images/generations',
         {
           prompt: prompt,
-          n: 1,
-          model: model,
-          size: size,
+          n: modelParameters.n ? modelParameters.n : 1,
+          model: model ? model : 'dall-e-3',
+          size: modelParameters.n ? modelParameters.size : '1024x1024',
         },
         {
           headers: {
@@ -20,10 +23,27 @@ module.exports = {
         }
       );
 
-      const imageUrl = response.data.data[0].url;
       
-      if (response.status === 200) {
-        return response.data;
+      if (response.status === 200 && response.data.data) {
+         
+          for (let index = 0; index < response.data.data.length; index++) {
+              const imageUrl = response.data.data[index].url;
+
+            // Salva a imagem no repositório
+              console.log(`Enviando imagem ${index + 1} para o repositório de imagens...`);
+              const savedImage = await imageRepoAPI.createImage(
+                imageUrl, // url da imagem
+                {}, // Metadados adicionais
+                '.jpg', // Extensão do arquivo
+                '73c6f20e-441e-4739-b42c-10c262138fdd', // Chave da API do Image Repo
+                1, // Configuração do FTP
+                false // Conteúdo em Base64
+              );
+              savedImages.push(savedImage);
+          }
+
+          console.log('Todas as imagens válidas foram salvas com sucesso!');
+          return savedImages; // Retorna as imagens salvas
       } else {
         throw new Error(`Erro ao processar com DALL-E: ${response.statusText}`);
       }
